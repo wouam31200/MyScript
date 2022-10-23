3d0cfb5f22ed38c1d8abb430868ecb6ec287a933
wouam31200 <105128426+wouam31200@users.noreply.github.com>
2022-10-23 18:35:26 +0200

    Add files via upload

--- liello.sh
+++ liello.sh
@@ -19,7 +19,7 @@ echo "This part of the script will give the option to install Postfix instead of
 echo "Postifx is preferred as it produces better logs than Sendmail in case for the need to troubleshoot sending email issues. "
 echo "During installation of postfix, it will require some manual configurations, but the defaults will be ready to select."
 echo ""
-echo "You will only need to press the enter key to continue for the two questions, HOWEVER, it is reccomended you have the server" 
+echo "You will only need to press the enter key to continue for the two questions, HOWEVER, it is reccomended you have the server"
 echo "Fully Qualified Domain Name (FQDN)/hosname already set (e.g. server.myserver.com) before continuing beyond this point."
 echo ""
 echo "If you have not done so, the FQDN/hostname can be easily set, as an example, with the command: "
@@ -35,22 +35,22 @@ echo ""
 read -n 1 -p "Do you wish to continue with installation of Postfix? (y/n/q[uit]) "
                 if [ "$REPLY"   = "y" ]; then
 
-			echo ""
-			echo "Now installing Postfix"
-			echo ""
-			apt -y install postfix
-		elif [ "$REPLY"   = "n" ]; then
-			echo ""
-			echo "Now installing Sendmail"
-			echo ""
-			apt -y install sendmail
-		elif [ "$REPLY"   = "q" ]; then
-			echo ""
-			echo "Exiting"
-			echo ""
-			exit 0
+            echo ""
+            echo "Now installing Postfix"
+            echo ""
+            apt -y install postfix
+        elif [ "$REPLY"   = "n" ]; then
+            echo ""
+            echo "Now installing Sendmail"
+            echo ""
+            apt -y install sendmail
+        elif [ "$REPLY"   = "q" ]; then
+            echo ""
+            echo "Exiting"
+            echo ""
+            exit 0
 
-		fi
+        fi
 
 } #end of install_postfix
 
@@ -105,7 +105,7 @@ install_monit ()
 
 #Monit is not is Buster Backports
 
-apt install -t buster-backports monit -y 
+apt install -t buster-backports monit -y
 
 read -p "Enter a Notification email address for sytem monitor: ${EMAIL}"
 
