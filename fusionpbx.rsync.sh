3848f1ff7a9bac0fec45f4673c2ac703401149c6
wouam31200 <105128426+wouam31200@users.noreply.github.com>
2023-01-07 19:38:59 +0100

    Create fusionpbx.rsync.sh

---
+++ fusionpbx.rsync.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+#!/bin/sh
+now=$(date +%Y-%m-%d)
+ssh_server=1.2.3.4
+database_host=127.0.0.1
+database_port=5432
+export PGPASSWORD="serveur.backup"
+
+# Chemin vers le fichier contenant le mot de passe root
+PASSWORD_FILE="/home/pass.txt"
+
+# Si le fichier de mot de passe n'existe pas, on demande à l'utilisateur de le créer
+if [ ! -f $PASSWORD_FILE ]; then
+    echo "Le fichier de mot de passe n'a pas été trouvé !"
+    echo -n "Veuillez entrer le mot de passe root : "
+    read -s ROOT_PASSWORD
+    echo $ROOT_PASSWORD > $PASSWORD_FILE
+    chmod 600 $PASSWORD_FILE
+fi
+
+# Lecture du mot de passe à partir du fichier
+ROOT_PASSWORD=$(cat $PASSWORD_FILE)
+
+#run the remote backup
+ssh -p 22 root@$ssh_server "nice -n -20 /etc/cron.daily/./fusionpbx-backup" --password-file=<(echo $ROOT_PASSWORD)
+
+#delete freeswitch logs older 7 days
+find /var/log/freeswitch/freeswitch.log.* -mtime +7 -exec rm {} \;
+
+#synchronize the backup directory
+#rsync -avz -e 'ssh -p 22' root@$ssh_server:/var/backups/fusionpbx /var/backups --password-file=<(echo $ROOT_PASSWORD)
+rsync -avz -e 'ssh -p 22' root@$ssh_server:/var/backups/fusionpbx/postgresql /var/backups/fusionpbx --password-file=<(echo $ROOT_PASSWORD)
+rsync -avz -e 'ssh -p 22' root@$ssh_server:/var/www/fusionpbx /var/www --password-file=<(echo $ROOT_PASSWORD)
+rsync -avz -e 'ssh -p 22' root@$ssh_server:/etc/fusionpbx /etc --password-file=<(echo $ROOT_PASSWORD)
+find /var/backups/fusionpbx/postgresql -mtime +2 -exec rm {} \;
+
+rsync -avz -e 'ssh -p 22' root@$ssh_server:/etc/freeswitch/ /etc --password-file=<(echo $ROOT_PASSWORD)
+rsync -avz -e 'ssh -p 22' root@$ssh_server:/var/lib/freeswitch/storage /var/lib/freeswitch --password-file=<(echo $ROOT_PASSWORD)
+rsync -avz -e 'ssh -p 22' root@$ssh_server:/var/lib/freeswitch/recordings /var/lib/freeswitch --password-file=<(echo $ROOT_PASSWORD)
+rsync -avz -e 'ssh -p 22' root@$ssh_server:/usr/share/freeswitch/scripts /usr/share/freeswitch --password-file=<(echo $ROOT_PASSWORD)
+rsync -avz -e 'ssh -p 22' root@$ssh_server:/usr/share/freeswitch/sounds /usr/share/freeswitch --password-file=<(echo $ROOT_PASSWORD)
+
+echo "Restoring the Backup"
+#extract the backup from the tgz file
+#tar -xvpzf /var/backups/fusionpbx/backup_$now.tgz -C /
+
+#remove the old database
+psql --host=$database_host --port=$database_port  --username=fusionpbx -c 'drop schema public cascade;'
+psql --host=$database_host --port=$database_port  --username=fusionpbx -c 'create schema public;'
+#restore the database
+pg_restore -v -Fc --host=$database_host --port=$database_port --dbname=fusionpbx --username=fusionpbx /var/backups/fusionpbx/postgresql/fusionpbx_pgsql_$now.sql
+
+#restart freeswitch
+service freeswitch restart
+echo "Restore Complete";
